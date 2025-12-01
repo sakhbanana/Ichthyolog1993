85ef534092834b2641bcc18cad24785af667f2bf
sakhbanana <sakhbanana@gmail.com>
2025-12-01 04:50:59 +0000

    как я могу развернуть этот проект на своем смартфоне

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type {NextConfig} from 'next';
 
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -32,4 +38,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { FirebaseClientProvider } from '@/firebase';
 export const metadata: Metadata = {
   title: 'Ихтиологи 1993',
   description: 'Мессенджер для вашей группы.',
+  manifest: '/manifest.json',
+  themeColor: '#673AB7',
 };
 
 export default function RootLayout({
@@ -19,6 +21,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png"></link>
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
