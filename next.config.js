ffb3a355de29a02a24d5f24bc798ea42b025bf03
sakhbanana <sakhbanana@gmail.com>
2025-12-03 14:43:30 +0000

    fix8

--- next.config.js
+++ next.config.js
@@ -1,20 +1,22 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
+
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === "development",
+  buildExcludes: [/middleware-manifest\.json$/], // важно для output: export
 });
 
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
+  output: "export",  // заменяет next export
   images: {
     unoptimized: true,
     remotePatterns: [
-      { protocol: 'https', hostname: 'placehold.co', pathname: '/**' },
-      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'picsum.photos', pathname: '/**' },
+      { protocol: "https", hostname: "placehold.co", pathname: "/**" },
+      { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
+      { protocol: "https", hostname: "picsum.photos", pathname: "/**" },
     ],
   },
 };
