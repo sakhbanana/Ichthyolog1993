f7600611f7829593afd8a9e5661410ff2e16fb05
sakhbanana <sakhbanana@gmail.com>
2025-12-06 10:32:54 +1100

    Add storage bucket to Firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:412265599460:web:3526038a292d42db8037f1",
   "apiKey": "AIzaSyDkBbT9HQvD-vlAVI5cXB-kpGW4aNNA6Po",
   "authDomain": "studio-151048688-101d3.firebaseapp.com",
+  "storageBucket": "studio-151048688-101d3.appspot.com",
   "measurementId": "",
   "messagingSenderId": "412265599460"
 };
