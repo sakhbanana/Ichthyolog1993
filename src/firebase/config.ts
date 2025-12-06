2feeadab2b1f7747f79631c79d443e16e8515587
sakhbanana <sakhbanana@gmail.com>
2025-12-06 01:54:40 +0000

    fix4

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,6 @@ export const firebaseConfig = {
   "appId": "1:412265599460:web:3526038a292d42db8037f1",
   "apiKey": "AIzaSyDkBbT9HQvD-vlAVI5cXB-kpGW4aNNA6Po",
   "authDomain": "studio-151048688-101d3.firebaseapp.com",
-  "storageBucket": "studio-151048688-101d3.appspot.com",
   // Updated to match bucket shown in Firebase Console
   // Use the canonical bucket name to avoid CORS/bucket-not-found issues
   "storageBucket": "studio-151048688-101d3.firebasestorage.app",
